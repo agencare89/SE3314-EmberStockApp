a89a6d56f33bec3e642061c4ce1290d3f0c248e9
Emily Guglielmi <eguglie@uwo.ca>
2015-03-04 22:07:40 -0500

    liking to the bid orders and sell orders and showing views

--- router.js
+++ router.js
@@ -2,10 +2,12 @@
  * Created by Adam on 2015-03-01.
  */
 StockApp.Router.map(function() {
-    this.resource('application', {path: '/'});
-    this.resource('marketByOrder');
-    this.resource('marketByPrice');
-    this.resource('placeBidOrder');
-    this.resource('placeSaleOrder');
-    this.resource('stockStateSummary');
+    this.resource('application', {path: '/'}, function() {
+        this.resource('stockStateSummary', {path: '/'}, function() {
+            this.resource('marketByOrder');
+            this.resource('marketByPrice');
+            this.resource('placeBidOrder');
+            this.resource('placeSaleOrder');
+        });
+    });
 });
