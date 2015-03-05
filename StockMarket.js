f2eacac31ebdd5da712fd32158bac3f1d420d5f8
Emily Guglielmi <eguglie@uwo.ca>
2015-03-04 20:49:59 -0500

    fixed the routing so routes.js was functional

--- StockMarket.js
+++ StockMarket.js
@@ -1,26 +1,10 @@
 /**
  * Created by Adam on 2015-03-01.
  */
-window.App = Ember.Application.create({
+StockApp = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
 
-App.ApplicationSerializer = DS.LSSerializer.extend();
-App.ApplicationAdapter = DS.LSAdapter.extend({ namespace: 'StockMarket' });
+StockApp.ApplicationSerializer = DS.LSSerializer.extend();
+StockApp.ApplicationAdapter = DS.LSAdapter.extend({ namespace: 'StockMarket' });
 
-
-
-App.Router.map(function() {
-
-    this.resource('application', function() {
-
-        this.resource('stockStateSummary', function() {
-
-            this.resource('placeBidOrder');
-            this.resource('placeSaleOrder');
-            this.resource('marketByOrder');
-            this.resource('marketByPrice');
-
-        });
-    });
-});
--- router.js
+++ router.js
@@ -1,3 +1,11 @@
 /**
  * Created by Adam on 2015-03-01.
  */
+StockApp.Router.map(function() {
+    this.resource('application', {path: '/'});
+    this.resource('marketByOrder');
+    this.resource('marketByPrice');
+    this.resource('placeBidOrder');
+    this.resource('placeSaleOrder');
+    this.resource('stockStateSummary');
+});
