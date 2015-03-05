9c46d75a4ca955e7ba2aa183a19f9bac548e0850
agencare89 <agencare@uwo.ca>
2015-03-04 22:15:17 -0500

    lol

---
+++ models/company.js
@@ -0,0 +1,4 @@
+StockApp.Company = DS.Model.extend({
+    name: DS.attr(),
+    openPrice: DS.attr('number')
+});
--- router.js
+++ router.js
@@ -2,10 +2,13 @@
  * Created by Adam on 2015-03-01.
  */
 StockApp.Router.map(function() {
-    this.resource('application', {path: '/'});
-    this.resource('marketByOrder');
-    this.resource('marketByPrice');
-    this.resource('placeBidOrder');
-    this.resource('placeSaleOrder');
-    this.resource('stockStateSummary');
+    this.resource('application', {path: '/'}, function() {
+        this.resource('marketByOrder');
+        this.resource('marketByPrice');
+        this.resource('placeBidOrder');
+        this.resource('placeSaleOrder');
+        this.resource('stockStateSummary', {path: '/'});
+    });
+
+
 });
