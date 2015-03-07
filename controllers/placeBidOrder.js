3881e78f59ef1805f76dc8da62946ea61938cbe0
Emily Guglielmi <eguglie@uwo.ca>
2015-03-07 17:26:57 -0500

    added the proper functionality to the stock state summary

--- controllers/placeBidOrder.js
+++ controllers/placeBidOrder.js
@@ -1,39 +1,66 @@
 StockApp.PlaceBidOrderController = Ember.Controller.extend({
     actions: {
         newBid: function(params) {
-            // Create a new bid order and store it
-            var newBid = this.store.createRecord('buyOrder', {
-                companyID: params.id,
-                numOfShares: this.get('numOfShares'),
-                price: this.get('price'),
-                comp: params
-            });
-            newBid.save();
-            // Set the new changes into the 
-            params.set('volume', this.get('numOfShares'));
-            params.set('lastSale', this.get('price'));
-            var netChange = (params.get('lastSale')-params.get('openPrice')).toFixed(2);
-            var percentChange = (((params.get('lastSale')-params.get('openPrice'))/params.get('openPrice'))*100).toFixed(2);
-            params.set('netChange', netChange);
-            params.set('percentChange', percentChange);
-
-            if (netChange > 0) {
-                params.set('changePic', './img/up.png');
+            // Search through the sales to find if the bid can be met
+            var numSold = 0;
+            for (var i = 0; i < params.get('listOfSells').get('length'); i++) {
+                if (params.get('listOfSells').objectAt(i).get('price') <= this.get('price')) {
+                    if (parseInt(params.get('listOfSells').objectAt(i).get('numOfShares')) < parseInt(this.get('numOfShares'))) {
+                        numSold = parseInt(numSold + params.get('listOfSells').objectAt(i).get('numOfShares'));
+                        this.set('numOfShares', parseInt(this.get('numOfShares')) - parseInt(params.get('listOfSells').objectAt(i).get('numOfShares')));
+                        params.get('listOfSells').removeAt(i);
+                        i--;
+                    }
+                    else if (parseInt(params.get('listOfSells').objectAt(i).get('numOfShares')) == parseInt(this.get('numOfShares'))) {
+                        numSold = parseInt(numSold + params.get('listOfSells').objectAt(i).get('numOfShares'));
+                        this.set('numOfShares', 0);
+                        params.get('listOfSells').removeAt(i);
+                        break;
+                    }
+                    else if (parseInt(params.get('listOfSells').objectAt(i).get('numOfShares')) > parseInt(this.get('numOfShares'))){
+                        numSold = parseInt(numSold + this.get('numOfShares'));
+                        params.get('listOfSells').objectAt(i).set('numOfShares', parseInt(params.get('listOfSells').objectAt(i).get('numOfShares')) - parseInt(this.get('numOfShares')));
+                        this.set('numOfShares', 0);
+                        break;
+                    }
+                }
             }
-            else if(netChange < 0) {
-                params.set('changePic', './img/down.png');
+            if(parseInt(this.get('numOfShares')) != 0) {
+                // Create a new bid order and store it
+                var newBid = this.store.createRecord('buyOrder', {
+                    companyID: params.id,
+                    numOfShares: this.get('numOfShares'),
+                    price: this.get('price'),
+                    comp: params
+                });
+                newBid.save();
             }
-            else {
-                params.set('changePic', './img/noChange.png');
+            // Set the sale to show in stockStateSummary if there was a sale made
+            if (numSold > 0) {
+                params.set('volume', parseInt(params.get('volume')+numSold));
+                params.set('lastSale', this.get('price'));
+                var netChange = (params.get('lastSale') - params.get('openPrice')).toFixed(2);
+                var percentChange = (((params.get('lastSale') - params.get('openPrice')) / params.get('openPrice')) * 100).toFixed(2);
+                params.set('netChange', netChange);
+                params.set('percentChange', percentChange);
+                if (netChange > 0) {
+                    params.set('changePic', './img/up.png');
+                }
+                else if (netChange < 0) {
+                    params.set('changePic', './img/down.png');
+                }
+                else {
+                    params.set('changePic', './img/noChange.png');
+                }
             }
-
+            // Clear the order form and transition back to marketBy Order
             this.set('numOfShares', '');
             this.set('price', '');
             this.transitionToRoute('marketByOrder', params);
         },
 
-
         cancelBid: function() {
+            // Reset the page and transition back to view stockStateSummary
             this.set('numOfShares', '');
             this.set('price', '');
             this.transitionToRoute('stockStateSummary');
--- controllers/placeSaleOrder.js
+++ controllers/placeSaleOrder.js
@@ -1,20 +1,66 @@
 StockApp.PlaceSaleOrderController = Ember.Controller.extend({
     actions:{
         newSale: function(params) {
-            var newSale = this.store.createRecord('sellOrder', {
-                companyID: params.id,
-                numOfShares: this.get('numOfShares'),
-                price: this.get('price'),
-                comp: params
-            });
-            newSale.save();
-            params.set('volume', this.get('numOfShares'));
-            params.set('lastSale', this.get('price'));
+            // Search through the sales to find if the bid can be met
+            var numSold = 0;
+            console.log(params.get('listOfBuys').get('length'));
+            for (var i = 0; i < params.get('listOfBuys').get('length'); i++) {
+                console.log(params.get('listOfBuys').objectAt(i).get('price'));
+                console.log(this.get('price'));
+                if (params.get('listOfBuys').objectAt(i).get('price') >= this.get('price')) {
+                    if (parseInt(params.get('listOfBuys').objectAt(i).get('numOfShares')) < parseInt(this.get('numOfShares'))) {
+                        numSold = parseInt(numSold + params.get('listOfBuys').objectAt(i).get('numOfShares'));
+                        this.set('numOfShares', parseInt(this.get('numOfShares')) - parseInt(params.get('listOfBuys').objectAt(i).get('numOfShares')));
+                        params.get('listOfBuys').removeAt(i);
+                        i--;
+                    }
+                    else if (parseInt(params.get('listOfBuys').objectAt(i).get('numOfShares')) == parseInt(this.get('numOfShares'))) {
+                        numSold = parseInt(numSold + params.get('listOfBuys').objectAt(i).get('numOfShares'));
+                        this.set('numOfShares', 0);
+                        params.get('listOfBuys').removeAt(i);
+                        break;
+                    }
+                    else if (parseInt(params.get('listOfBuys').objectAt(i).get('numOfShares')) > parseInt(this.get('numOfShares'))){
+                        numSold = parseInt(numSold + this.get('numOfShares'));
+                        params.get('listOfBuys').objectAt(i).set('numOfShares', parseInt(params.get('listOfBuys').objectAt(i).get('numOfShares')) - parseInt(this.get('numOfShares')));
+                        this.set('numOfShares', 0);
+                        break;
+                    }
+                }
+            }
+            if(parseInt(this.get('numOfShares')) != 0) {
+                var newSale = this.store.createRecord('sellOrder', {
+                    companyID: params.get('id'),
+                    numOfShares: this.get('numOfShares'),
+                    price: this.get('price'),
+                    comp: params
+                });
+                newSale.save();
+            }
+            // Set the sale to show in stockStateSummary if there was a sale made
+            if (numSold > 0) {
+                console.log(numSold);
+                params.set('volume', parseInt(params.get('volume')+numSold));
+                params.set('lastSale', this.get('price'));
+                var netChange = (params.get('lastSale') - params.get('openPrice')).toFixed(2);
+                var percentChange = (((params.get('lastSale') - params.get('openPrice')) / params.get('openPrice')) * 100).toFixed(2);
+                params.set('netChange', netChange);
+                params.set('percentChange', percentChange);
+                if (netChange > 0) {
+                    params.set('changePic', './img/up.png');
+                }
+                else if (netChange < 0) {
+                    params.set('changePic', './img/down.png');
+                }
+                else {
+                    params.set('changePic', './img/noChange.png');
+                }
+            }
+            // Clear the order form and transition back to marketBy Order
             this.set('numOfShares', '');
             this.set('price', '');
             this.transitionToRoute('marketByOrder', params);
         },
-
 
         cancelSale: function() {
             this.set('numOfShares', '');
